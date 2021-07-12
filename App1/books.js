e82142547c76cd210edf639317eb7c9337452a02
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-07-13 01:01:40 +0530

    Add files via upload

---
+++ App1/books.js
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I love you to the moon",
+    author: "charles M",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
+    title: "Our Class is a Family",
+    author: "Brit Bennett",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71e5m7xQd0L._AC_UL200_SR200,200_.jpg",
+    title: "The Vanishing Half: A Novel",
+    author: "Shannon Olsen",
+  },
+];
